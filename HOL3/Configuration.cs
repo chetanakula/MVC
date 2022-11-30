3288ec10e9dc218fe40cfc523ae6b7be6befce7e
chetanakula <115332203+chetanakula@users.noreply.github.com>
2022-11-30 10:13:18 +0000

    HOL3

---
+++ HOL3/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace HOL3_Project.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<HOL3_Project.Models.MyDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(HOL3_Project.Models.MyDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ HOL3/MyDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace HOL3_Project.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext()
+            : base("name=conn")
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+    }
+
+}
