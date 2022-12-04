72438a0929534c9b4f83ee26efc22e9c5aeb41ec
chetanakula <115332203+chetanakula@users.noreply.github.com>
2022-12-04 14:53:14 +0000

    HOL6

---
+++ HOL6/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using HOL6.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HOL6.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetServerTime()
+        {
+            System.Threading.Thread.Sleep(5000);
+            return PartialView();
+        }
+        static List<User> users = new List<User>();
+        public ActionResult GetAllUsers()
+        {
+            return PartialView(users);
+        }
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateUser(User u)
+        {
+            users.Add(u);
+            return RedirectToAction("GetAllUsers");
+        }
+
+
+    }
+}
